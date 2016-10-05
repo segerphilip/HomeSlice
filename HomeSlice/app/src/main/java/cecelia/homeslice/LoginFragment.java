20e1baf50f3b32aaa6605e6bccd2b9ec42edf3ca
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-10-05 18:56:15 -0400

    ADD: framework start

---
+++ HomeSlice/app/src/main/java/cecelia/homeslice/LoginFragment.java
@@ -0,0 +1,13 @@
+package cecelia.homeslice;
+
+import android.app.Fragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginFragment extends Fragment {
+
+
+
+
+}
--- HomeSlice/app/src/main/java/cecelia/homeslice/MainActivity.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/MainActivity.java
@@ -1,5 +1,7 @@
 package cecelia.homeslice;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addMainPageFragment();
+    }
+
+    public void addMainPageFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //Add the MainActivityFragment as the starting Fragment when the app is opened
+        fragmentTransaction.replace(R.id.fragment_holder, new LoginFragment(), "CURRENT_FRAGMENT");
+        fragmentTransaction.commit();
     }
 }
