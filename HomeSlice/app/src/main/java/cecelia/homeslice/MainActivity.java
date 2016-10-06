7318c7feb6acb011c56746a6a0d53f013fb0e3e3
Philip Seger <seger.philip@gmail.com>
2016-10-06 19:10:42 -0400

    ADD: firebase starter

--- HomeSlice/app/src/main/java/cecelia/homeslice/MainActivity.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
