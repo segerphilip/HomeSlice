2e1e34752996706269ae4d8467de16fa735a3924
Philip Seger <seger.philip@gmail.com>
2016-10-09 16:11:40 -0400

    DELETE: restarting work, dead branch

--- HomeSlice/app/src/main/java/cecelia/homeslice/CookActivity.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/CookActivity.java
@@ -1,11 +1,9 @@
 package cecelia.homeslice;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class CookActivity extends AppCompatActivity {
 
@@ -17,14 +15,10 @@ public class CookActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "WOW WOWW", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final CookActivityFragment newFragment = new CookActivityFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_holder, newFragment, "fragment_cook");
+        transaction.commit();
     }
 
 }
