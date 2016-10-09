5c0c4e89bfd9d7129c5d79254980b79b24a8329d
Philip Seger <seger.philip@gmail.com>
2016-10-09 13:00:25 -0400

    ADD: tablayout started

--- HomeSlice/app/src/main/java/cecelia/homeslice/CookActivity.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/CookActivity.java
@@ -13,6 +13,7 @@ public class CookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cook);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
