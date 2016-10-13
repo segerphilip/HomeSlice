a1d6c78ba90417ac6003147d31be7eeb40ff9262
segerphilip <seger.philip@gmail.com>
2016-10-12 20:11:33 -0400

    EDIT: fragment started for specific menu, needs work

--- HomeSlice/app/src/main/java/cecelia/homeslice/CookMenuItemAdapter.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/CookMenuItemAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,8 +17,7 @@ import butterknife.ButterKnife;
  * Adapter used to display ingredients and cooking instructions for a food
  */
 public class CookMenuItemAdapter extends ArrayAdapter<CookMenuItem> {
-    @BindView(R.id.cook_item_text) TextView itemText;
-    @BindView(R.id.cook_item_subtext) TextView itemSubText;
+    @BindView(R.id.menu_instructions) TextView menuInstructions;
 
     public CookMenuItemAdapter(Context context, ArrayList<CookMenuItem> items) {
         super(context, 0, items);
@@ -30,7 +30,7 @@ public class CookMenuItemAdapter extends ArrayAdapter<CookMenuItem> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.cook_specific_menu, parent, false);
         }
         ButterKnife.bind(this, convertView);
-        itemText.setText(item.getItem() + "BASLDFHAS");
+        menuInstructions.setText(item.getInstructions());
 
         return convertView;
     }
--- HomeSlice/app/src/main/java/cecelia/homeslice/CookSpecificMenuFragment.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/CookSpecificMenuFragment.java
@@ -2,7 +2,10 @@ package cecelia.homeslice;
 
 import android.support.v4.app.Fragment;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 
@@ -13,6 +16,9 @@ public class CookSpecificMenuFragment extends Fragment {
     @BindView(R.id.imageView) ImageView menuImage;
     @BindView(R.id.menu_title) TextView menuTitle;
     @BindView(R.id.menu_ingredients) TextView menuIngredients;
-//    @BindView(R.id.menu_instructions) ListView menuInstructions;
+    @BindView(R.id.menu_instructions) ListView menuList;
+
+    private ArrayList<CookMenuItem> menu;
+    private CookMenuListAdapter ordersAdapter;
 
 }
