2d169a4abf3034359e8cd7d3e5bff6e47e0c292e
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-10-13 21:22:31 -0400

    EDIT: cleaning up the code a bit

--- HomeSlice/app/src/main/java/cecelia/homeslice/Order.java
+++ HomeSlice/app/src/main/java/cecelia/homeslice/Order.java
@@ -33,30 +33,6 @@ public class Order {
         addToOrderDatabaseRef(item, orderRef);
     }
 
-    public void add(ArrayList<OrderItem> items, DatabaseReference orderRef) {
-
-        this.items.addAll(items);
-        for (OrderItem item: this.items) {
-            addToOrderDatabaseRef(item, orderRef);
-        }
-    }
-
-    public void remove(int index, DatabaseReference orderRef) {
-        OrderItem item = this.items.get(index);
-        this.items.remove(index);
-        removeFromDatabase(item, orderRef);
-    }
-
-
-    private void removeFromDatabase(OrderItem item, DatabaseReference orderRef) {
-        orderRef.child("items").child(item.dbId).removeValue();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("orderitems").child(item.dbId);
-        dbRef.removeValue();
-    }
-
-    private void addToOrderDatabaseRef(OrderItem item, DatabaseReference orderRef) {
-        orderRef.child("items").child(item.dbId).setValue(item);
-    }
 
     public void remove(OrderItem item, DatabaseReference orderRef) {
 
@@ -75,43 +51,23 @@ public class Order {
             }
             index = index + 1;
         }
-
     }
 
-    public void remove(MenuItem item) {
-        boolean removed = false;
-        int index = 0;
-
-        while (!removed) {
-            OrderItem orderItem = this.items.get(index);
-            if (orderItem.menuItem.getName() == item.getName()) {
-                this.items.remove(orderItem);
-                removed = true;
-            }
-            index++;
-        }
-        if (!removed) {
-            throw new IllegalArgumentException("no such menuitem to remove from order");
-        }
+    private void removeFromDatabase(OrderItem item, DatabaseReference orderRef) {
+        orderRef.child("items").child(item.dbId).removeValue();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("orderitems").child(item.dbId);
+        dbRef.removeValue();
     }
 
-
-    public ArrayList<OrderItem> getItems() {
-        return this.items;
+    private void addToOrderDatabaseRef(OrderItem item, DatabaseReference orderRef) {
+        orderRef.child("items").child(item.dbId).setValue(item);
     }
 
     public int getStatus() { return this.status; }
 
     public void setStatus(int status) { this.status = status; }
 
-    public boolean isInOrder(MenuItem item) {
-        for (OrderItem o: this.items) {
-            if (o.menuItem.getName().equals(item.getName())) {
-                return true;
-            }
-        }
-        return false;
-    }
+    public ArrayList<OrderItem> getItems() { return this.items; }
 
     public OrderItem getItem(MenuItem item) {
         for (OrderItem o: this.items) {
@@ -122,20 +78,27 @@ public class Order {
         throw new Error("that menu menuItem does not exist in the order");
     }
 
+    public boolean isInOrder(MenuItem item) {
+        for (OrderItem o: this.items) {
+            if (o.menuItem.getName().equals(item.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static Order getFromDataSnapshot(DataSnapshot dataSnapshot) {
         HashMap<String, Object> dataSnapshotValue = (HashMap<String, Object>) dataSnapshot.getValue();
-        ArrayList<OrderItem> items = new ArrayList<OrderItem>();
+        ArrayList<OrderItem> items = new ArrayList<>();
         if (dataSnapshotValue != null) {
             HashMap<String, Object> itemsMap = (HashMap<String, Object>)dataSnapshotValue.get("items");
             if (dataSnapshotValue.get("items") != null) {
                 setOrderItemsfromDataSnapshot(itemsMap, items);
             }
-            //pip getting null status
             int status = Integer.valueOf(dataSnapshotValue.get("status").toString());
             Order order = new Order(items, status);
             return order;
         }
-
         return null;
     }
 
